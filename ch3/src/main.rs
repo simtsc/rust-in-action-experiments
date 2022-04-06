a83380299000c2d87ea9bc0c2f8b60edf7c96700
simtsc <2539637+simtsc@users.noreply.github.com>
2022-04-06 19:18:10 +0000

    Added File struct

--- ch3/src/main.rs
+++ ch3/src/main.rs
@@ -1,6 +1,10 @@
 #![allow(unused_variables)]
 
-type File = String;
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+}
 
 fn open(f: &mut File) -> bool {
     true
@@ -11,13 +15,29 @@ fn close(f: &mut File) -> bool {
 }
 
 #[allow(dead_code)]
-fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
-    unimplemented!()
+fn read(f: &mut File, save_to: &mut Vec<u8>) -> usize {
+    let mut tmp = f.data.clone();
+    let read_length = tmp.len();
+    save_to.reserve(read_length);
+    save_to.append(&mut tmp);
+    read_length
 }
 
 fn main() {
-    let mut f1 = File::from("f1.txt");
+    let mut f1 = File {
+        name: String::from("f1.txt"),
+        data: vec![114, 117, 115, 116, 33],
+    };
+
+    let mut buffer: Vec<u8> = vec![];
+
     open(&mut f1);
-    //read(&mut f1, vec![]);
+    let f1_length = read(&mut f1, &mut buffer);
     close(&mut f1);
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f1);
+    println!("{} is {} bytes long", &f1.name, f1_length);
+    println!("{}", text)
 }
