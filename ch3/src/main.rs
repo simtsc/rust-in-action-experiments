3a12d5f4a6134a54ad26adca572b2e9d9e9069c7
simtsc <2539637+simtsc@users.noreply.github.com>
2022-04-06 19:47:17 +0000

    changed to impl and Error checking Result

--- ch3/src/main.rs
+++ ch3/src/main.rs
@@ -1,39 +1,61 @@
 #![allow(unused_variables)]
 
+#[derive(Debug,PartialEq)]
+enum FileState {
+    Open,
+    Closed,
+}
+
 #[derive(Debug)]
 struct File {
     name: String,
     data: Vec<u8>,
+    state: FileState,
 }
 
-fn open(f: &mut File) -> bool {
-    true
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+            state: FileState::Closed,
+        }
+    }
+
+    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        if self.state != FileState::Open {
+            return Err(String::from("File must be open for reading"));
+        }
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        Ok(read_length)
+    }
 }
 
-fn close(f: &mut File) -> bool {
-    true
+fn open(mut f: File) -> Result<File, String> {
+    f.state = FileState::Open;
+    Ok(f)
 }
 
-#[allow(dead_code)]
-fn read(f: &mut File, save_to: &mut Vec<u8>) -> usize {
-    let mut tmp = f.data.clone();
-    let read_length = tmp.len();
-    save_to.reserve(read_length);
-    save_to.append(&mut tmp);
-    read_length
+fn close(mut f: File) -> Result<File, String> {
+    f.state = FileState::Closed;
+    Ok(f)
 }
 
 fn main() {
-    let mut f1 = File {
-        name: String::from("f1.txt"),
-        data: vec![114, 117, 115, 116, 33],
-    };
+    let mut f1 = File::new("f1.txt");
 
     let mut buffer: Vec<u8> = vec![];
 
-    open(&mut f1);
-    let f1_length = read(&mut f1, &mut buffer);
-    close(&mut f1);
+    if f1.read(&mut buffer).is_err() {
+        println!("Error checking is working")
+    }
+
+    f1 = open(f1).unwrap();
+    let f1_length = f1.read(&mut buffer).unwrap();
+    f1 = close(f1).unwrap();
 
     let text = String::from_utf8_lossy(&buffer);
 
